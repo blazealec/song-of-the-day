f59059e88ed82f7727d41c1ad45634ce52210c38
vboxuser <vboxuser@Trixie.sfcc.edu>
2025-05-14 19:48:37 +0000

    Add fc:frame meta tag for Farcaster Mini App compatibility

--- pages/miniapp.js
+++ pages/miniapp.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useEffect } from 'react';
 import App from '../src/App';
 
@@ -10,5 +11,12 @@ export default function MiniAppPage() {
     });
   }, []);
 
-  return <App />;
+  return (
+    <>
+      <Head>
+        <meta property="fc:frame" content='{"version":"next","imageUrl":"https://song-of-the-day-sepia.vercel.app/og-image.png","button":{"title":"Start Quiz","action":{"type":"launch_frame","name":"Song of the Day","url":"https://song-of-the-day-sepia.vercel.app/miniapp","splashImageUrl":"https://song-of-the-day-sepia.vercel.app/splash.png","splashBackgroundColor":"#000000"}}}' />
+      </Head>
+      <App />
+    </>
+  );
 } 
